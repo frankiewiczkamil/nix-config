95a0aea11d564a3ccf2568f2f6a4d952e85f9a82
kamil <frankiewiczkamil@gmail.com>
2024-11-10 21:34:31 +0100

    Add `git` config :wrench:

--- home.nix
+++ home.nix
@@ -39,6 +39,20 @@
       '';
       initExtraBeforeCompInit = builtins.readFile ./zshrc;
     };
+    git = {
+      enable = true;
+      userName = "kamil";
+      userEmail = "frankiewiczkamil@gmail.com";
+      signing = {
+        key = "0x12A95E73B631A6FF";
+        signByDefault = true;
+      };
+      extraConfig = {
+        init = {
+          defaultBranch = "main";
+        };
+      };
+    };
   };
 
   home.file.".p10k.zsh".text = builtins.readFile ./p10k.zsh;
