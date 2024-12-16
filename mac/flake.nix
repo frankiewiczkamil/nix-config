f22df2d8c8fddcc6d295702e6ab5483282fc27b6
kamil <frankiewiczkamil@gmail.com>
2024-12-16 20:59:38 +0100

    Upgrade  `nixpkgs.url` to 24.11 :arrow_up:

--- mac/flake.nix
+++ mac/flake.nix
@@ -1,13 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
 
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
